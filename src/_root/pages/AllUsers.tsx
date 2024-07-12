5d4e08827ce66e6e93cabfcc816b01e7f27ba0fa
Shehper Sameer Ahmed <32867470+Sameer1122@users.noreply.github.com>
2024-07-12 17:31:12 +0500

    'some-message'

--- src/_root/pages/AllUsers.tsx
+++ src/_root/pages/AllUsers.tsx
@@ -9,21 +9,22 @@ const AllUsers = () => {
 
   if (isErrorCreators) {
     toast({ title: "Something went wrong." });
-    
+
     return;
   }
 
   return (
     <div className="common-container">
       <div className="user-container">
-      <h2 className="h3-bold md:h2-bold text-left w-full border-b border-gray-300 pb-2">Grow Community</h2>
-
+        <h2 className="h3-bold md:h2-bold text-left w-full border-b border-gray-300 pb-2">
+          Grow Community
+        </h2>
 
         {isLoading && !creators ? (
           <Loader />
         ) : (
           <ul className="user-grid">
-            {creators?.documents.map((creator) => (
+            {creators?.map((creator) => (
               <li key={creator?.$id} className="flex-1 min-w-[200px] w-full  ">
                 <UserCard user={creator} />
               </li>
