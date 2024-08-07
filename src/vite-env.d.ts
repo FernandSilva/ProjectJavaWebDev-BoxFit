92a9dbccca07ddadbbec2afbe8ab7b8d4fd71ca9
FernandoSilva <fernand00.silva0@gmail.com>
2024-08-08 08:44:31 +0900

    Updated vite error

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,21 @@
-///<reference types= "vite/Client" />
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_APPWRITE_URL: string;
+    readonly VITE_APPWRITE_PROJECT_ID: string;
+    readonly VITE_APPWRITE_DATABASE_ID: string;
+    readonly VITE_APPWRITE_STORAGE_ID: string;
+    readonly VITE_APPWRITE_USER_COLLECTION_ID: string;
+    readonly VITE_APPWRITE_POST_COLLECTION_ID: string;
+    readonly VITE_APPWRITE_SAVES_COLLECTION_ID: string;
+    readonly VITE_APPWRITE_USER_RELATIONSHIPS_COLLECTION_ID: string;
+    readonly VITE_APPWRITE_USER_COMMENTS_COLLECTION_ID: string;
+    readonly VITE_APPWRITE_MESSAGES_COLLECTION_ID: string;
+    readonly VITE_APPWRITE_FUNCTION_KEY: string;
+    readonly VITE_APPWRITE_GPT_KEY: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+  
