9e243c251ec8154d27ddbc0a1e27206aceacebd7
FernandoSilva <fernand00.silva0@gmail.com>
2025-04-17 18:18:42 +0200

    new logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 // src/App.tsx
+import { Route, Routes } from "react-router-dom";
 import SigninForm from "@/_auth/forms/SigninForm";
 import SignupForm from "@/_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+
 import {
   AllUsers,
   Chat,
@@ -11,33 +15,30 @@ import {
   PostDetails,
   Profile,
   Saved,
-  Settings // Correct import here
-  ,
+  Settings,
   UpdateProfile,
   Notification,
 } from "@/_root/pages";
+
 import { Toaster } from "@/components/ui/toaster";
-import { Route, Routes } from "react-router-dom";
-import AuthLayout from "./_auth/AuthLayout";
-import RootLayout from "./_root/RootLayout";
 
 import "./globals.css";
-import 'swiper/css/bundle';
+import "swiper/css/bundle";
 
 const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
-        {/* public routes */}
+        {/* Public Routes */}
         <Route element={<AuthLayout />}>
           <Route path="/sign-in" element={<SigninForm />} />
           <Route path="/sign-up" element={<SignupForm />} />
         </Route>
 
-        {/* private routes */}
+        {/* Private Routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
-          <Route path="/explore" element={<Explore  />} />
+          <Route path="/explore" element={<Explore />} />
           <Route path="/saved" element={<Saved />} />
           <Route path="/all-users" element={<AllUsers />} />
           <Route path="/create-post" element={<CreatePost />} />
@@ -45,8 +46,8 @@ const App = () => {
           <Route path="/posts/:id" element={<PostDetails />} />
           <Route path="/profile/:id/*" element={<Profile />} />
           <Route path="/update-profile/:id" element={<UpdateProfile />} />
-          <Route path="/Chat" element={<Chat />} />
-          <Route path="/settings" element={<Settings />} /> {/* Correct route here */}
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/settings" element={<Settings />} />
           <Route path="/notifications" element={<Notification />} />
         </Route>
       </Routes>
